4f31b68a2e692a3b4ca7b60941106c9443754024
Nola Stowe <nola@rubygeek.com>
2008-06-08 22:18:02 -0500

    deployment files

---
+++ config/deploy.rb
@@ -0,0 +1,22 @@
+default_run_options[:pty] = true 
+set :application, "bakeryman2"
+set :repository,  "git@github.com:rubygeek/bakeryman.git"
+set :user, 'nola'
+
+# If you aren't deploying to /u/apps/#{application} on the target
+# servers (which is the default), you can specify the actual location
+# via the :deploy_to variable:
+set :deploy_to, "/home/nola/#{application}"
+set :scm, :git
+set :deploy_via, 'copy'
+
+role :app, "nickandnola.com"
+role :web, "nickandnola.com"
+role :db,  "nickandnola.com", :primary => true
+
+after :deploy, :railsplayground_link
+
+
+task :railsplayground_link do
+  run "cd /home/nola/www; ln -s /home/nola/#{application}/current/public #{application}"
+end
