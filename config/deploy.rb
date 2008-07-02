3e463984f7dd92ae48c09435123af164dad745d2
Nola Stowe <nstowe@68-29-7-204.area4.spcsdns.net>
2008-07-02 07:31:59 -0500

    fixed path

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,5 +19,5 @@ after :deploy, :railsplayground_link
 
 task :railsplayground_link do
   run "cd /home/nola/www; ln -s /home/nola/#{application}/current/public #{application}"
-  run "chmod 755 /home/nola/#{application}/public/dispatch.cgi"
+  run "chmod 755 /home/nola/#{application}/current/public/dispatch.cgi"
 end
